ae988bba54628b8252a1ab4679e6165e8294cdeb
Shakir <shakirkhan72727@gmail.com>
2025-08-27 17:44:13 +0500

    integrate generally

---
+++ container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
+
+export default () => {
+  let ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
