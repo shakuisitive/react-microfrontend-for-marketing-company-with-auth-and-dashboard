33aed11811c7358dcb378530941ea2067f8725cc
Shakir <shakirkhan72727@gmail.com>
2025-08-31 17:38:28 +0500

    implement multi tier navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,23 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   let ref = useRef(null);
+  let history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    let { onParentNavigate } = mount(ref.current, {
+      onNavigate({ pathname: nextPathName }) {
+        let { pathname } = history.location;
+
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory } from "history";
 
-function mount(el) {
-  ReactDOM.render(<App />, el);
+function mount(el, { onNavigate }) {
+  let history = createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      if (history.pathname !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
 }
 
 if (process.env.NODE_ENV === "development") {
   let devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
