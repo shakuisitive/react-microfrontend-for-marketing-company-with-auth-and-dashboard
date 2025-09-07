d9c91b1314dbc0129ebac95ec9d31303ccc7fd4e
Shakir <shakirkhan72727@gmail.com>
2025-09-08 03:34:10 +0500

    add auth app to remotes property in ModuleFederationPlugin for container

---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,28 @@
+import { mount } from "auth/AuthApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export default () => {
+  let ref = useRef(null);
+  let history = useHistory();
+  // this history object will give us the ability to change container's browser history path
+
+  useEffect(() => {
+    let { onParentNavigate } = mount(ref.current, {
+      onNavigate(location) {
+        // the location parameter will give us the object
+        // that will give us path of the remote app i.e. auth app
+        let { pathname: newPathName } = location;
+        let pathname = history.location;
+
+        if (pathname !== newPathName) {
+          history.push(newPathName);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
+  return <div ref={ref} />;
+};
