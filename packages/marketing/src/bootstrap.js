b240d8c54b27a4f8728a870c2a029ab067dc74fe
Shakir <shakirkhan72727@gmail.com>
2025-09-08 01:04:35 +0500

    make remote apps use browser history when being ran in isolation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-function mount(el, { onNavigate }) {
-  let history = createMemoryHistory();
+function mount(el, { onNavigate, defaultHistory }) {
+  let history = defaultHistory || createMemoryHistory();
 
   // doing below will trigger a change in container
   // each time there's a change in this marketing remote app
@@ -29,7 +29,9 @@ if (process.env.NODE_ENV === "development") {
   let devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot, {});
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory(),
+    });
   }
 }
 
