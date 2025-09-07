a6b91fe0b7d29bbfaf61324af2149b5eb69fb378
Shakir <shakirkhan72727@gmail.com>
2025-09-08 04:37:59 +0500

    add auth yml for deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,6 +16,7 @@ let prodConfig = {
       name: "container",
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
